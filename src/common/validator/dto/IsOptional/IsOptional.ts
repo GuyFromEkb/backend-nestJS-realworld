8f142eacf27a5867440482fa0238cdb85f22a0ec
GuyFromEkb <guyFromEkb@example.com>
2023-10-13 23:10:38 +0500

    feat: IsOptional декоратор, который не принемает null (по дефолту)

---
+++ src/common/validator/dto/IsOptional/IsOptional.ts
@@ -0,0 +1,19 @@
+import { IsOptional as IsOptionalValidator, ValidateIf, ValidationOptions } from "class-validator";
+
+/**
+ * Checks if value is missing and if so, ignores all validators.
+ *
+ * @param nullable If `true`, all other validators will be skipped even when the value is `null`. `false` by default.
+ * @param validationOptions {@link ValidationOptions}
+ *
+ * @see IsOptional exported from `class-validator.
+ */
+export function IsOptional(nullable = false, validationOptions?: ValidationOptions) {
+  if (nullable) {
+    return IsOptionalValidator(validationOptions);
+  }
+
+  return ValidateIf((ob: any, v: any) => {
+    return v !== undefined;
+  }, validationOptions);
+}
---
+++ src/common/validator/dto/index.ts
@@ -0,0 +1,1 @@
+export { IsOptional } from "./IsOptional/IsOptional";
